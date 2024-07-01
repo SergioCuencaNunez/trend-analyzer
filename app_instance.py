76c0c505e5b0704a3eceeecf48d90def366ccc5f
Sergio <scuenca06@gmail.com>
2024-07-01 03:03:41 +0200

    deploy live fix

--- app_instance.py
+++ app_instance.py
@@ -1,12 +1,14 @@
 import dash
 import dash_bootstrap_components as dbc
+from dash import dcc, html
+from dash.dependencies import Input, Output
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title = "TrendAnalyzer"
-app._favicon = 'favicon.ico'  # Referencia al favicon
+app._favicon = 'favicon.ico'  # Reference to the favicon
 
 app.index_string = '''
 <!DOCTYPE html>
@@ -33,3 +35,77 @@ app.index_string = '''
 '''
 
 app.config.suppress_callback_exceptions = True
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    dbc.Navbar(
+        dbc.Container(
+            [
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src='/assets/logo-bright.png', height='60px')),
+                    ],
+                    align="center",
+                    className="g-0",
+                ),
+                dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+                dbc.Collapse(
+                    dbc.Nav(
+                        [
+                            dbc.NavItem(dcc.Link('HOME', href='/', className='nav-link', style={'margin-right': '20px'})),
+                            dbc.NavItem(dcc.Link('FORECAST', href='/forecast', className='nav-link', style={'margin-right': '20px'})),
+                            dbc.NavItem(dcc.Link('ABOUT', href='/about', className='nav-link', style={'margin-right': '20px'})),
+                        ],
+                        className="ms-auto",
+                        navbar=True,
+                    ),
+                    id="navbar-collapse",
+                    navbar=True,
+                ),
+            ]
+        ),
+        color="#050A30",
+        dark=True,
+        className="navbar navbar-expand-md navbar-dark",
+    ),
+    html.Div(id='page-content', className='main-content'),
+    html.Footer([
+        html.P([
+            dcc.Link('Terms and Conditions', href='/terms', className='footer-link'),
+            " | Sergio Cuenca Núñez, June 2024 | Version: 1.0"
+        ], style={'text-align': 'center'}),
+    ], className="footer")
+])
+
+# Validation layout to include all sub-apps' layouts
+def load_validation_layout():
+    import forecast
+    import home
+    import about
+    import terms
+    return html.Div([
+        forecast.layout,
+        home.layout,
+        about.layout,
+        terms.layout
+    ])
+
+app.validation_layout = load_validation_layout()
+
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def display_page(pathname):
+    import forecast
+    import home
+    import about
+    import terms
+    
+    if pathname == '/':
+        return home.layout
+    elif pathname == '/forecast':
+        return forecast.layout
+    elif pathname == '/about':
+        return about.layout
+    elif pathname == '/terms':
+        return terms.layout
+    else:
+        return "404 Page Not Found"
