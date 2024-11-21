9d17d83aea961a8b09a63d3332b13693ca76f77e
Sergio <scuenca06@gmail.com>
2024-11-21 01:59:41 +0100

    minor fixes

--- src/app.py
+++ src/app.py
@@ -119,5 +119,5 @@ news_callbacks(app)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
-    app.run_server(debug = True)
-    #app.run_server(host='0.0.0.0', port=port)
+    #app.run_server(debug = True)
+    app.run_server(host='0.0.0.0', port=port)
